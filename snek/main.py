fe07ffd80940e85c31b7900909cf81bcc1b4e562
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-11-15 08:34:57 -0800

    Fetch and parse dependencies for an example package

---
+++ snek/main.py
@@ -0,0 +1,13 @@
+import requests
+from packaging.requirements import Requirement
+from typing import Iterable
+
+REPOSITORY_URL = 'https://www.pypi.org'
+
+if __name__ == '__main__':
+    package_name: str = 'flask'
+    json: dict = requests.get(f"{REPOSITORY_URL}/pypi/{package_name}/json").json()
+    deps: Iterable[str] = json['info']['requires_dist']
+    deps: Iterable[Requirement] = map(lambda dep: Requirement(dep), deps)
+    for dep in deps:
+        print(dep)
