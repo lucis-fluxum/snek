1a6e8852b53326faee6e77027db0904b2415aa5e
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-11-17 13:41:31 -0800

    Recursively add new requirements, some refactoring

--- snek/resolver.py
+++ snek/resolver.py
@@ -1,7 +1,7 @@
 import logging
 from functools import reduce
 from pprint import pp
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Set
 
 import requests
 from packaging.markers import Marker, UndefinedEnvironmentName
@@ -16,7 +16,7 @@ log = logging.getLogger(__name__)
 
 class Resolver:
     @staticmethod
-    def fetch_requirement(name: str) -> dict:
+    def fetch_metadata(name: str) -> dict:
         response = requests.get(f"{REPOSITORY_URL}/pypi/{name}/json")
         if response:
             return response.json()
@@ -28,17 +28,22 @@ class Resolver:
         if not requirements:
             raise RuntimeError('No requirements given.')
         name = requirements[0].name
-        available_versions: Dict[str, list] = Resolver.fetch_requirement(name)['releases']
+        available_versions: Dict[str, list] = Resolver.fetch_metadata(name)['releases']
         merged_requirements = reduce(SpecifierSet.__and__, map(lambda r: r.specifier, requirements))
         return [Version(v) for v in available_versions if v in merged_requirements]
 
-    def __init__(self, requirement: Requirement, dependencies: List[Requirement] = []):
+    def __init__(self, requirement: Optional[Requirement] = None,
+                 extras: Set[str] = {},
+                 dependencies: List[Requirement] = []):
+        self.dependencies: List[Requirement] = dependencies
         self._requirement = requirement
-        self._extras = requirement.extras
-        self._dependencies: List[Requirement] = dependencies
+        self._extras = extras
+        if requirement:
+            self._extras = requirement.extras
+            self.add_new_requirement(requirement)
 
-    def get_requirements(self) -> List[Requirement]:
-        metadata = Resolver.fetch_requirement(self._requirement.name)
+    def get_sub_requirements(self, requirement: Requirement) -> List[Requirement]:
+        metadata = Resolver.fetch_metadata(requirement.name)
         requires_dist: List[str] = metadata['info']['requires_dist']
         if requires_dist and len(requires_dist) > 0:
             reqs = [Requirement(dep) for dep in requires_dist]
@@ -48,19 +53,28 @@ class Resolver:
 
     def add_new_requirement(self, new_requirement: Requirement):
         if not self.evaluate_marker(new_requirement.marker):
-            log.warning(f"Incompatible marker: {new_requirement.marker}")
+            log.warning(f"Incompatible marker: {new_requirement.marker}, ignoring {new_requirement}")
             return
 
-        for existing_requirement in self._dependencies:
-            if existing_requirement.name.lower() == new_requirement.name.lower():
-                compatible_versions = Resolver.get_compatible_versions(existing_requirement, new_requirement)
-                log.debug(f"{new_requirement.name}: {compatible_versions}")
-                if compatible_versions:
-                    existing_requirement.specifier = existing_requirement.specifier & new_requirement.specifier
-                    log.debug(f"Requirement added: {str(existing_requirement)}")
-                    return
-                else:
-                    log.critical(f"No compatible versions found for {new_requirement.name}.")
+        if new_requirement.name not in map(lambda r: r.name, self.dependencies):
+            print(f"Adding {new_requirement}...")
+            if Resolver.get_compatible_versions(new_requirement):
+                self.dependencies.append(new_requirement)
+                for sub_requirement in self.get_sub_requirements(new_requirement):
+                    self.add_new_requirement(sub_requirement)
+            else:
+                raise RuntimeError(f"No compatible versions found for {new_requirement.name}.")
+        else:
+            for existing_requirement in self.dependencies:
+                if existing_requirement.name.lower() == new_requirement.name.lower():
+                    compatible_versions = Resolver.get_compatible_versions(existing_requirement, new_requirement)
+                    log.debug(f"Compatible versions for {new_requirement.name}: {compatible_versions}")
+                    if compatible_versions:
+                        existing_requirement.specifier = existing_requirement.specifier & new_requirement.specifier
+                        log.debug(f"Requirement updated: {str(existing_requirement)}")
+                        return
+                    else:
+                        raise RuntimeError(f"No compatible versions found for {new_requirement.name}.")
 
     def evaluate_marker(self, marker: Optional[Marker]) -> bool:
         try:
@@ -76,8 +90,8 @@ class Resolver:
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    resolver = Resolver(Requirement('Flask[dev]'))
-    resolver._dependencies = resolver.get_requirements()
-    resolver.add_new_requirement(Requirement('Werkzeug <= 0.15.6; extra == "dev"'))
-    pp(resolver._dependencies)
+    # logging.basicConfig(level=logging.DEBUG)
+    resolver = Resolver()
+    resolver.add_new_requirement(Requirement('Flask'))
+    print('Final list of dependencies:')
+    pp(resolver.dependencies)
--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -24,31 +24,33 @@ def mock_fetch_requirement(mocker):
     metadatas = {
         'Flask': json.loads(load_fixture('pypi_Flask.json'))
     }
-    mocker.patch('snek.resolver.Resolver.fetch_requirement', side_effect=metadatas.__getitem__)
+    mocker.patch('snek.resolver.Resolver.fetch_metadata', side_effect=metadatas.__getitem__)
 
 
 class TestResolver:
-    @pytest.mark.parametrize('req_str, expected_reqs',
-                             [('Flask', FLASK_REQUIREMENTS), ('Flask[dev]', FLASK_DEV_REQUIREMENTS),
-                              ('Flask[docs]', FLASK_DOCS_REQUIREMENTS), ('Flask[dev, docs]', FLASK_ALL_REQUIREMENTS)])
-    def test_get_requirements(self, mocker, req_str, expected_reqs):
-        resolver = Resolver(Requirement(req_str))
+    @pytest.mark.parametrize('req_str, extras, expected_reqs',
+                             [('Flask', {}, FLASK_REQUIREMENTS),
+                              ('Flask[dev]', {'dev'}, FLASK_DEV_REQUIREMENTS),
+                              ('Flask[docs]', {'docs'}, FLASK_DOCS_REQUIREMENTS),
+                              ('Flask[dev, docs]', {'dev', 'docs'}, FLASK_ALL_REQUIREMENTS)])
+    def test_get_sub_requirements(self, mocker, req_str, extras, expected_reqs):
+        resolver = Resolver(extras=extras)
         mock_fetch_requirement(mocker)
-        requirements = map(str, resolver.get_requirements())
+        requirements = map(str, resolver.get_sub_requirements(Requirement(req_str)))
         assert set(requirements) == expected_reqs
 
     def test_evaluate_extra(self):
-        resolver_no_extra = Resolver(Requirement('Flask'))
+        resolver_no_extra = Resolver()
         assert not resolver_no_extra.evaluate_marker(Marker("extra == 'dev'"))
-        resolver_one_extra = Resolver(Requirement('Flask[dev]'))
+        resolver_one_extra = Resolver(extras={'dev'})
         assert resolver_one_extra.evaluate_marker(Marker("extra == 'dev'"))
         assert not resolver_one_extra.evaluate_marker(Marker("extra == 'another'"))
-        resolver_multi_extra = Resolver(Requirement('Flask[dev, test, another]'))
+        resolver_multi_extra = Resolver(extras={'dev', 'test', 'another'})
         for extra in ['dev', 'test', 'another']:
             assert resolver_multi_extra.evaluate_marker(Marker(f"extra == '{extra}'"))
 
     def test_evaluate_marker(self):
-        resolver = Resolver(Requirement('Flask'))
+        resolver = Resolver()
         assert resolver.evaluate_marker(None)
         windows_marker = Marker('sys_platform=="win32"')
         if sys.platform == 'win32':
@@ -57,7 +59,7 @@ class TestResolver:
             assert not resolver.evaluate_marker(windows_marker)
 
     def test_get_compatible_versions(self, mocker):
-        resolver = Resolver(Requirement('Flask'))
+        resolver = Resolver()
         mock_fetch_requirement(mocker)
         assert len(resolver.get_compatible_versions(Requirement('Flask'))) == 32
         assert len(resolver.get_compatible_versions(Requirement('Flask'),
@@ -67,3 +69,7 @@ class TestResolver:
                                                     Requirement('Flask <= 1.1'))) == 5
         assert len(resolver.get_compatible_versions(Requirement('Flask'),
                                                     Requirement('Flask ~= 1.0'))) == 7
+
+    def test_add_new_requirement(self, mocker):
+        pass
+        # resolver = Resolver(Requirement('Flask'))
