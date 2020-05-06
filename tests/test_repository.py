1fd549e5de5eb2d8dd495e743ad1226cba093ab7
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-06 12:18:55 -0700

    Clean up repository tests

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -8,12 +8,8 @@ class TestRepository:
         mock_repository_json(mocker)
         repo = Repository()
         assert len(repo.get_compatible_versions(Requirement('Flask'))) == 32
-        assert len(repo.get_compatible_versions(Requirement('Flask'),
-                                                Requirement('Flask > 1.0'))) == 6
-        assert len(repo.get_compatible_versions(Requirement('Flask'),
-                                                Requirement('Flask > 1.0'),
+        assert len(repo.get_compatible_versions(Requirement('Flask'), Requirement('Flask > 1.0'))) == 6
+        assert len(repo.get_compatible_versions(Requirement('Flask'), Requirement('Flask > 1.0'),
                                                 Requirement('Flask <= 1.1'))) == 5
-        assert len(repo.get_compatible_versions(Requirement('Flask'),
-                                                Requirement('Flask ~= 1.0'))) == 7
-        assert len(repo.get_compatible_versions(Requirement('Flask > 1'),
-                                                Requirement('Flask < 1'))) == 0
+        assert len(repo.get_compatible_versions(Requirement('Flask'), Requirement('Flask ~= 1.0'))) == 7
+        assert len(repo.get_compatible_versions(Requirement('Flask > 1'), Requirement('Flask < 1'))) == 0
