69f50949ae54ac4bf91e1266f1b474b730e218f1
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-05-06 13:31:42 -0700

    Create empty reducer tests

---
+++ tests/test_reducer.py
@@ -0,0 +1,7 @@
+class TestReducer:
+    pass
+    # TODO: Write this once we're able to get a compiled list from the reducer
+    # def test_reduce(self, mocker):
+    #     mock_repository_json(mocker)
+    #     resolver = Resolver()
+    #     reducer = Reducer()
